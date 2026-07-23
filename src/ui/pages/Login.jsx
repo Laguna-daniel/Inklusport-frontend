e84e7130060d523d4c5f6e86375c2d4ede9ff559
Laguna-daniel <lagunabuenodanielalejandro@gmail.com>
2026-07-23 11:03:26 -0500

    Feat :deleted content innecesary

--- src/ui/pages/Login.jsx
+++ src/ui/pages/Login.jsx
@@ -721,7 +721,6 @@ const Login = () => {
         <div className="hero-panel">
           <div className="hero-overlay"></div>
           <div className="hero-content">
-            <span className="highlight-text">BIENVENIDO DE NUEVO</span>
             <h1 className="hero-title">
               Supera tus<br />límites cada<br />día.
             </h1>
@@ -804,8 +803,8 @@ const Login = () => {
 
             {/* Botón Iniciar Sesión */}
             <button type="submit" className="submit-btn">
-              <span>INICIAR SESIÓN</span>
-              <span>&gt;</span>
+              <span>ACCEDER </span>
+              
             </button>
           </form>
 
@@ -821,11 +820,7 @@ const Login = () => {
                   <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.06l3.66 2.84c.87-2.6 3.3-4.52 6.16-4.52z" />
                 </svg>
               </button>
-              <button type="button" className="social-btn" title="Facebook">
-                <svg width="18" height="18" viewBox="0 0 24 24" fill="#1877F2">
-                  <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
-                </svg>
-              </button>
+
               <button type="button" className="social-btn" title="Apple ID">
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="#000000">
                   <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M15.97 6.32c.62-.75 1.04-1.8 0.92-2.85-.9.04-2 .6-2.65 1.35-.58.67-1.09 1.75-.95 2.78 1.01.08 2.05-.53 2.68-1.28z"/>
